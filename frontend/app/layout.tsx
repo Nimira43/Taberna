f1429ceaa9f58a0897531adbaed580ae3479e2c9
Nimira43 <n.rabone72@aol.com>
2024-11-22 07:20:23 +0000

    Wrapper children in layout with AppRouterCacheProvider

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { AppRouterCacheProvider} from '@mui/material-nextjs/v13-appRouter'
 import { Poppins } from 'next/font/google'
 import './globals.css'
 
@@ -20,7 +21,9 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={poppins.className}>
+        <AppRouterCacheProvider>
         {children}
+        </AppRouterCacheProvider>
       </body>
     </html>
   )
