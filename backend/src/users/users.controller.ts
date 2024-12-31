483b6fd54c87f8ff13071bbf9209d53bcdd9156f
Nimira43 <n.rabone72@aol.com>
2024-12-31 12:13:39 +0000

    Exporting CreateUserRequest and brought into users.controller.ts

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,7 +1,8 @@
 import { Body, Controller, Post } from '@nestjs/common'
+import { CreateUserRequest } from './dto/create-user.request';
 
 @Controller('users')
 export class UsersController {
   @Post()
-  createUser(@Body() request: ) {}
+  createUser(@Body() request: CreateUserRequest) {}
 }
