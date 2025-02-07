9e8b974462617f7d1ae140efe3b05b7229f3f05c
Nimira43 <n.rabone72@aol.com>
2025-02-07 23:17:28 +0000

    Testing out new new theme colours and Tailwind - all good.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,8 @@
 export default function page() {
   return (
-    <div>
-      <h1 className='logo'>Taberna</h1>
-      <hr />
-      <p>Online Trading Post</p>
+    <div className='bg-light p-4 mt-4 rounded-lg text-center font-semibold'>
+      <h1 className='logo text-primary'>Taberna</h1>
+      <p className='text-grey-dark uppercase font-normal'>Online Trading Post</p>
     </div>
   )
 }
